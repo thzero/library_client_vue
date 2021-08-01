783b727b29b8df1647994b99b3eb4fc9b10314e7
thzero <corvette@thzero.com>
2021-08-01 14:58:18 -0500

    cleanup updates

--- boot/baseGameSystems.js
+++ boot/baseGameSystems.js
@@ -1,7 +1,7 @@
 class BaseGameSystems {
 	// eslint-disable-next-line
-	async execute(Vue, app, router, store) {
-		this._initialize(Vue);
+	async execute(framework, app, router, store) {
+		this._initialize(framework);
 	}
 
 	// eslint-disable-next-line
